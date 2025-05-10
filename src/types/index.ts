604e0aae50c49e589172b2c528092e9961e05cb1
Pranto Deb <rajprit0m@outlook.com>
2025-05-10 22:20:48 +0600

    [Types]

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export type TServices = {
+  _id: string;
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  currency: string;
+  image: string;
+  category: string;
+  tags: string[];
+};
