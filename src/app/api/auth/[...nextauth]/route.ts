c1579089a2e43c456e22ee475bf3e4be4b28aa22
Pranto Deb <rajprit0m@outlook.com>
2025-05-12 21:14:58 +0600

    [Moved auth options]

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,6 @@
-import { loginUser } from "@/app/actions/auth/loginUser";
+import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        if (!credentials) {
-          console.log("No credentials received.");
-          return null;
-        }
-        const result = await loginUser({
-          email: credentials.email,
-          password: credentials.password,
-        });
-
-        if (result.success && result.user) {
-          const userDoc = result.user;
-
-          return {
-            id: userDoc._id.toString(),
-            name: userDoc.name || userDoc.email,
-            email: userDoc.email,
-          };
-        }
-
-        return null;
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  pages: {
-    signIn: "/login",
-  },
-};
 
 const handler = NextAuth(authOptions);
 
