24763e9c8a93c34c3f2e73a8b97f99d49bbce845
Pranto Deb <rajprit0m@outlook.com>
2025-05-12 23:39:21 +0600

    [#]

--- src/components/PagesCom/GG.tsx
+++
@@ -1,9 +0,0 @@
-const GG = () => {
-    return (
-        <div>
-            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus minima in, qui unde ea error. Distinctio excepturi tenetur aperiam iure maiores similique dolor fugiat, illo soluta animi cupiditate iusto praesentium!</h1>
-        </div>
-    );
-};
-
-export default GG;
