5e1577b8a5ed86425cdfad27b9e491663f093e4a
Pranto Deb <rajprit0m@outlook.com>
2025-05-18 03:07:00 +0600

    [fetch my booking]

--- src/app/api/service/route.ts
+++ src/app/api/service/route.ts
@@ -1,5 +1,20 @@
+import { authOptions } from "@/lib/authOptions";
 import dbConnect, { COLLECTION_NAME } from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
+
+export const GET = async (req: Request) => {
+  const session = await getServerSession({ req, ...authOptions });
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const email = session.user?.email as string;
+  const bookingCollection = await dbConnect(COLLECTION_NAME.BOOKING);
+  const result = await bookingCollection.find({ email }).toArray();
+  return NextResponse.json(result);
+};
 
 export const POST = async (req: Request) => {
   const body = await req.json();
